94d5220257a5ff39b268368eeb848157d55ae702
PotatoPost <postalex11@icloud.com>
2020-11-13 15:04:25 +0000

    update with loops

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     System.out.println("Welcome to PotatoPost's dumb programming thing!");
-    System.out.println("Version 3.0: Cool Things with Strings™");
+    System.out.println("Version 4.0: Now with loops!");
     System.out.println("Loading..."); //this doesnt actually load anything, i just do it so it looks cool lmao
 
     try {
@@ -22,59 +22,63 @@ class Main {
     long imStupid; //this variable is because i was stupid and concatenated two numbers together.
     byte programType; //this variable is used to determine what the program is gonna do. like, you know, it chooses the stuff. you know.
     String stringThing = ""; //its a string thing
+    boolean continueLoop = true; //determines if we want to continue the loop or not
 
     try {
-      System.out.println("\nSelect what you want to do (Enter the number):\n1.)Calculator Stuff\n2.)Cool Things with Strings™");
-      programType = scan.nextByte();
-      switch(programType){
-        case 1:
-          System.out.println("\nSelect what you want to do (Enter the number):\n1.)Add two numbers\n2.)Add two numbers, but even cooler");
-          programType = scan.nextByte();
-          switch(programType){
-            case 1:
-              System.out.println("\nEnter a number:");
-              thingy = scan.nextLong();
-              mathStuff.setValue(thingy, 1);
+      while (continueLoop) {
+        System.out.println("\nSelect what you want to do (Enter the number):\n1.)Calculator Stuff\n2.)Cool Things with Strings™");
+        programType = scan.nextByte();
+        switch(programType){
+          case 1:
+            System.out.println("\nSelect what you want to do (Enter the number):\n1.)Add two numbers\n2.)Add two numbers, but even cooler");
+            programType = scan.nextByte();
+            switch(programType){
+              case 1:
+                System.out.println("\nEnter a number:");
+                thingy = scan.nextLong();
+                mathStuff.setValue(thingy, 1);
 
-              System.out.println("\nEnter another number:");
-              thingy = scan.nextLong();
-              mathStuff.setValue(thingy, 2);
+                System.out.println("\nEnter another number:");
+                thingy = scan.nextLong();
+                mathStuff.setValue(thingy, 2);
 
-              imStupid = mathStuff.x + mathStuff.y;
-              System.out.println("\nThe two numbers added together are: " + imStupid);
-              break;
+                imStupid = mathStuff.x + mathStuff.y;
+                System.out.println("\nThe two numbers added together are: " + imStupid);
+                break;
 
-            case 2:
-              System.out.println("\nEnter a number:");
-              thingy = scan.nextLong(); //thingy is the scanner object to get input
-              System.out.println("\nWhat number do you want to add by?");
-              adder = scan.nextLong(); //adder adds the number, or something
-              mathStuff.thisFunctionThatIUseAlot(thingy, adder, 1);
+              case 2:
+                System.out.println("\nEnter a number:");
+                thingy = scan.nextLong(); //thingy is the scanner object to get input
+                System.out.println("\nWhat number do you want to add by?");
+                adder = scan.nextLong(); //adder adds the number, or something
+                mathStuff.thisFunctionThatIUseAlot(thingy, adder, 1);
 
-              System.out.println("\nEnter another number:");
-              thingy = scan.nextInt();
-              System.out.println("\nWhat Number do you want to add by?");
-              adder = scan.nextInt();
-              mathStuff.thisFunctionThatIUseAlot(thingy, adder, 2);
+                System.out.println("\nEnter another number:");
+                thingy = scan.nextInt();
+                System.out.println("\nWhat Number do you want to add by?");
+                adder = scan.nextInt();
+                mathStuff.thisFunctionThatIUseAlot(thingy, adder, 2);
 
-              imStupid = mathStuff.x + mathStuff.y;
-              System.out.println("The two numbers added together are: " + imStupid);
-              break;
-          }
-          break;
+                imStupid = mathStuff.x + mathStuff.y;
+                System.out.println("The two numbers added together are: " + imStupid);
+                break;
+            }
+            break;
 
-        case 2:
-          System.out.println("\nSelect what you want to do (Enter the number):\n1.)Idk how to describe this one, it just yells at you a bunch");
-          programType = scan.nextByte();
-          switch(programType){
-            case 1:
-              System.out.println("Enter a string: ");
-              stringThing = scan.next();
-              stringJazz.thisIsBad(stringThing);
-              break;
-          }
-
+          case 2:
+            System.out.println("\nSelect what you want to do (Enter the number):\n1.)Idk how to describe this one, it just yells at you a bunch");
+            programType = scan.nextByte();
+            switch(programType){
+              case 1:
+                System.out.println("\nEnter a string: ");
+                stringThing = scan.next();
+                stringJazz.thisIsBad(stringThing);
+                break;
+            }
         }
+        System.out.println("If you want to continue, type \"true\". Otherwise, type \"false\".");
+          continueLoop = scan.nextBoolean();
+      }
     } finally {
       scan.close();
     }
